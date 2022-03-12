d87f6d34b329f6db65da9eb7898e856bac03146d
Nind <dnsuser20112016@gmail.com>
2022-03-12 18:06:37 +1000

    добавили анимацию

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rb;
+    Animator animator;
+    bool isGrounded;
+    public float velocity;
+    public float jumpHeight;
+    public Transform groundCheck;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Horizontal") == 0 && isGrounded) 
+        {
+            animator.SetInteger("state", 1);
+        }
+        Flip();
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(transform.up * jumpHeight, ForceMode2D.Impulse);
+        }
+        GroundCheck();
+    }
+
+    // Вращение игрока
+    void Flip()
+    {
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            transform.localRotation = Quaternion.Euler(0, 0, 0);
+        }
+        else if (Input.GetAxis("Horizontal") < 0)
+        {
+            transform.localRotation = Quaternion.Euler(0, 180, 0);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(Input.GetAxis("Horizontal") * velocity, rb.velocity.y); 
+    }
+
+    void GroundCheck()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheck.position, 0.2f);
+        isGrounded = colliders.Length > 1;
+    }
+
+
+}
